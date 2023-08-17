74f3684bbce950aafbf1a5a608137623dba7003e
Sergio <sergiomolinaflores1@gmail.com>
2023-08-17 19:54:13 -0400

    se implementa funcion de envio de mail, mediante float button

--- app/src/main/java/cl/samf/phonenew/presentation/PhoneDetailsFragment.kt
+++ app/src/main/java/cl/samf/phonenew/presentation/PhoneDetailsFragment.kt
@@ -1,5 +1,7 @@
 package cl.samf.phonenew.presentation
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -45,9 +47,11 @@ class PhoneDetailsFragment : Fragment() {
     }
 
     private fun initLoadDate() {
-        //
+
 
         phoneViewModel.phoneIdLiveData(param1!!.toInt()).observe(viewLifecycleOwner) {
+            val name = it.name
+            val id = it.id.toString()
             if (it != null) {
                 binding.textViewTitleDetails.text = it.name
                 binding.imageViewDetails.load(it.image)
@@ -59,16 +63,48 @@ class PhoneDetailsFragment : Fragment() {
                 binding.textViewLastPrice.text = "$$formattedPricelasPrice"
                 binding.textViewPriceDetails.text = "$$formattedPrice"
 
-                if(it.credit){
+                if (it.credit) {
                     binding.textViewCreditDetails.text = "Acepta Tarjeta de Credito"
-                }else{
+                } else {
                     binding.textViewCreditDetails.text = "No acepta Tarjeta de Credito"
+                }
+                binding.floatingActionButton.setOnClickListener {
+
+                    sendMail(name, id)
+
                 }
             }
         }
+    }
 
+    private fun sendMail(name: String, id: String) {
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:")  // Indicamos que es para enviar un correo
+        intent.putExtra(
+            Intent.EXTRA_EMAIL,
+            arrayOf("destinatario@example.com")
+        )  // Destinatario
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Consulta " + name + " id " + id)  // Asunto
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "hola vi el telefono " + name + " de codigo" + id + "y me gustaria que me contactaran a este correo"
+        )  // Contenido del correo
+
+        try {
+            startActivity(
+                Intent.createChooser(
+                    intent,
+                    "Enviar correo con"
+                )
+            )  // Mostrar opciones de aplicaciones para enviar correo
+        } catch (e: Exception) {
+
+            // Manejar posibles errores o excepciones
+        }
     }
 }
+
+
 
 
 
